f01792aaaa8a4c433843efbddbf8c401269171e2
lylblog <52353581+lylblog@users.noreply.github.com>
2023-10-26 15:24:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.listen(8088, () => {
     console.log('服务启动')
 })
 
+let login = false;
+//如果未登录，返回未登录，否则，继续向下匹配，回调函数接收三个参数，最后一个是next,继续向下执行，路径一定要写在最上面，不然会先被test捕捉到，test没有执行next,就会捕捉不到请求。
+app.all('*', (req, res, next) => {
+    if(!login) return res.json('未登录')
+    next()
+})
+app.post('/test', (req, res) => {
+    res.json('test')
+})
+
 app.get('/', (req, res) => {
     res.send('<div>hello world</div>')
 })
@@ -13,3 +23,5 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     res.json('<div>hello login</div>')
 })
+
+
