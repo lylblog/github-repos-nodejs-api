87804214abac54b378e2985adf942edc385d47e7
lylblog <52353581+lylblog@users.noreply.github.com>
2023-10-26 15:09:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const PORT = process.env.PORT || 8081;
+const express = require('express')
+const app = express()
 
-export default function handler(req, res) {
-    var verify_result = false;
-    if(req.method === 'POST'){
-        const pwd_req = req.body['pwd'];
-        if(pwd_req === '!QAZ2wsx') {
-			verify_result = true;
-		} else { 
-			verify_result = false;
-		}
-	var res_data = {'verify_result': verify_result};
-    }
-    
+app.listen(8088, () => {
+    console.log('服务启动')
+})
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(res_data);
-    
-}
-
-
-
-// app.get('/api', async (req, res) => {
-//     const verify_result = false;
-//     const userinfo = req.body
-//      console.log(userinfo);
-//     // const user_req = req.query.username;
-//     if(user_req === '1') {
-     
-//       status=200,
-//       message=='登录成功',
-//       verify_result = true,
-
-//     } else { 
-//       status=400,
-//       message=='密码错误',
-//       verify_result = false,
-//      }
-
-//      res.setHeader("Access-Control-Allow-Origin", "*");
-// });
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+app.get('/', (req, res) => {
+    res.send('<div>hello world</div>')
+})
