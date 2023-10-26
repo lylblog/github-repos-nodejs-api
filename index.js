77484beabed28f671f0f72ccb447c7c8d59915c3
lylblog <52353581+lylblog@users.noreply.github.com>
2023-10-26 11:01:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,26 +21,27 @@ export default function handler(req, res) {
     
 };
 
-// app.get('/', async (req, res) => {
-//   const username = req.query.username || 'myogeshchavan97';
-//   try {
-//     const result = await axios.get(
-//       `https://api.github.com/users/${username}/repos`
-//     );
-//     const repos = result.data
-//       .map((repo) => ({
-//         name: repo.name,
-//         url: repo.html_url,
-//         description: repo.description,
-//         stars: repo.stargazers_count
-//       }))
-//       .sort((a, b) => b.stars - a.stars);
-
-//     res.send(repos);
-//   } catch (error) {
-//     res.status(400).send('Error while getting list of repositories');
-//   }
-// });
+app.get('/', async (req, res) => {
+   const verify_result = false;
+	res.status(200).send(verify_result);
+  //  const username = req.query.username;
+  //  try {
+  //    const result = await axios.get(
+  //      `https://api.github.com/users/${username}/repos`
+  //   );
+  //  const repos = result.data
+  //      .map((repo) => ({
+  //      name: repo.name,
+  //      url: repo.html_url,
+  //      description: repo.description,
+  //      stars: repo.stargazers_count
+  //    }))
+  //   .sort((a, b) => b.stars - a.stars);
+  //   res.send(repos);
+  // } catch (error) {
+  //   res.status(400).send('Error while getting list of repositories');
+  // }
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
