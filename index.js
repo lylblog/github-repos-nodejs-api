b086f433caf8c5bb80d47234705d6d3b0458a53e
lylblog <52353581+lylblog@users.noreply.github.com>
2023-10-26 11:35:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,16 +26,10 @@
 const express = require('express')
 
  
-// 导入加密模块
-const bcrypt = require('bcryptjs')
- 
 // 创建路由对象
 const router = express.Router()
  
-// 导入JWT
-const jwt = require('jsonwebtoken')
-// 定义密钥
-const jwtSecreKey = 'notbald'
+
  
  
 // 登录接口
@@ -46,12 +40,9 @@ router.post('/login',(req,res)=>{
         if(!compareResult) return res.send({status:1,message:'密码错误'})
         // 生成token字符串，展开运算符，剔除密码和用户头像
         const user = {...results[0],password:'',user_pic:''}
-        // console.log(user);
+        console.log(user);
         
-        // 生成 Token 字符串
-        const tokenStr = jwt.sign(user, jwtSecreKey, {
-        expiresIn: '10h', // token 有效期为 10 个小时
-            })
+
             
             res.send({
                 status: 0,
